b9aade95b51966f0ab53e5c9e30498780116846c
preethid <e0203237@u.nus.edu>
2018-10-02 23:54:21 +0800

    add tests for email command trial

--- src/main/java/seedu/address/logic/commands/EmailCommand.java
+++ src/main/java/seedu/address/logic/commands/EmailCommand.java
@@ -5,8 +5,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
 import seedu.address.commons.core.Messages;
+import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 
